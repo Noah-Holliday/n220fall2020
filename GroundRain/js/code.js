46a7147058e6e828218df41acc5b05025038a546
Noah-Holliday <40368053+Noah-Holliday@users.noreply.github.com>
2021-09-01 18:44:31 -0400

    Works with array of drops
    Might come back to create manager class

--- GroundRain/js/code.js
+++ GroundRain/js/code.js
@@ -1,42 +1,52 @@
 function setup() {
-    createCanvas(800, 600)
+    createCanvas(800, 600);
+    fill("blue");
+    raindropholder = [];
+    for (i=0; i<30; i++) {
+        newraindrop = new Raindrop();
+        raindropholder[i] = newraindrop;
+    }
 }
 
 function draw() {
-    background(200)
-    checkground.call(Ground)
-}
-
-class RainManager {
-    for()
+    background(200);
+    checkground.call(Ground);
+    for (i=0; i<30; i++){
+        raindropholder[i].falling();
+    }
 }
 
 class Raindrop {
     startx = Math.random() * 800
-    startrad = Math.random() * 5
-    dropspeed = Math.random() * 3
+    startrad = 2 + Math.random() * 5
+    dropspeed = 3 + Math.random() * 3
     height = 0 
-    body = circle(startx, height, startrad)
     falling = function() {
-        this.height += dropspeed
+        this.height += this.dropspeed
+        circle(this.startx, this.height, this.startrad)
+        if (this.height > Ground.y) {
+            this.height = 0,
+            this.startx = Math.random() * 800
+            this.startrad = 2 + Math.random() * 5
+            this.dropspeed = 3 + Math.random() * 3
+            Ground.raindropcontainer += 1;
+        }
     } 
 }
 
-function checkrain()
-
 var Ground  = {
-    height = 0,
-    width = 800,
-    y = 600,
-    x = 0,
-    raindropcontainer = 0,
+    height: 1,
+    width: 800,
+    y: 599,
+    x: 0,
+    raindropcontainer: 0
 }
 
 function checkground() {
-    rectangle(this.x, this.y, this.width, this.height)
+    rect(this.x, this.y, this.width, this.height)
     if (this.raindropcontainer > 10) {
         this.y -= 1,
-        this.height -= 1,
+        this.height += 1,
         this.raindropcontainer = 0
     }
 }
