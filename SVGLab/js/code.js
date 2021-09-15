7187f85db1a3aa9dc4c35beaabdaf561da5607c8
Noah-Holliday <40368053+Noah-Holliday@users.noreply.github.com>
2021-09-14 23:44:55 -0400

    SVG lab completion

--- SVGLab/js/code.js
+++ SVGLab/js/code.js
@@ -4,17 +4,55 @@ class Game {
     searchColor = '#99FF00';
     normalColor = '#7700AA';
     gameZone = document.getElementById("gameZone");
+    foundBar = new FoundBar();
 
     constructor() {
         for(var i = 0; i < 25; i++) {
             let newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
             newCircle.classList.add("gameCircle");
-            newCircle.setAttribute("cx", Math.random() * 400)
+            newCircle.setAttribute("cx", Math.random() * 600)
             newCircle.setAttribute("cy", Math.random() * 400)
+
+            if(Math.random() < .3) {
+                newCircle.dataset.hiddenColor = this.searchColor;
+                this.totalCircles ++;
+            } else {
+                newCircle.dataset.hiddenColor = this.normalColor;
+            }
+
+            newCircle.addEventListener("mouseover", (event) => {
+                event.target.style.fill = event.target.dataset.hiddenColor;
+            })
+
+            newCircle.addEventListener("mouseout", (event) => {
+                event.target.style.fill = "#000";
+            })
+
+            newCircle.addEventListener("click", (event) => {
+                if(event.target.dataset.hiddenColor == this.searchColor) {
+                    event.target.remove();
+
+                    this.foundCircles++;
+
+                    this.foundBar.setPercent(this.foundCircles / this.totalCircles);
+                }
+            })
+
 
             this.gameZone.appendChild(newCircle);
         }
+    }
+}
+
+class FoundBar {
+    element = document.getElementById("foundBar");
+    maxSize = 500;
+    percent = 0;
+    
+    setPercent(percent) {
+        this.percent = percent;
+        this.element.setAttribute("width", this.percent * this.maxSize);
     }
 }
 
