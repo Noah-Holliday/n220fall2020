6abb96c0049c653151ca4c368d71f9fa9648273f
Noah-Holliday <40368053+Noah-Holliday@users.noreply.github.com>
2021-09-30 18:29:44 -0400

    react project

--- WashingMachineBuilder/js/code.js
+++ WashingMachineBuilder/js/code.js
@@ -1,52 +1,103 @@
-washingZone = document.getElementById("washingZone");
-
 class WashingMachine {
-    loadingcircle = {x:250, y:350, r:60}
-    dialcircle = {x:350, y:175, r:20}
-    rectangle = {x:100, y:100, width:300 , height:400}
+    washingZone = document.getElementById("washingZone");
+
+    constructor(loadingcircle, dialcircle, rectangle){
+        this.loadingcircle = loadingcircle;
+        this.dialcircle = dialcircle;
+        this.rectangle = rectangle
+
+        if (Math.random() < .5) {
+            this.loadingcircle.x = this.loadingcircle.x + (Math.random() * 10)
+            this.loadingcircle.y = this.loadingcircle.y + (Math.random() * 10)
+            this.loadingcircle.r = this.loadingcircle.r + (Math.random() * 1)
+            this.dialcircle.x = this.dialcircle.x + (Math.random() * 10)
+            this.dialcircle.y = this.dialcircle.y + (Math.random() * 10)
+            this.dialcircle.r = this.dialcircle.r + (Math.random() * 1)
+            this.rectangle.x = this.rectangle.x + (Math.random() * 10)
+            this.rectangle.y = this.rectangle.y + (Math.random() * 10)
+            this.rectangle.width = this.rectangle.width + (Math.random() * 10)
+            this.rectangle.height = this.rectangle.height + (Math.random() * 10)
+        }
+        else {
+            this.loadingcircle.x = this.loadingcircle.x - (Math.random() * 10)
+            this.loadingcircle.y = this.loadingcircle.y - (Math.random() * 10)
+            this.loadingcircle.r = this.loadingcircle.r - (Math.random() * 1)
+            this.dialcircle.x = this.dialcircle.x - (Math.random() * 10)
+            this.dialcircle.y = this.dialcircle.y - (Math.random() * 10)
+            this.dialcircle.r = this.dialcircle.r - (Math.random() * 1)
+            this.rectangle.x = this.rectangle.x - (Math.random() * 10)
+            this.rectangle.y = this.rectangle.y - (Math.random() * 10)
+            this.rectangle.width = this.rectangle.width - (Math.random() * 10)
+            this.rectangle.height = this.rectangle.height - (Math.random() * 10)
+        }
+    }
+
+    baseRectangle = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    loadingCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    dialCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
     draw() {
-        let baseRectangle = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        let loadingCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        let dialCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        while (this.washingZone.firstChild) {
+            this.washingZone.removeChild(this.washingZone.firstChild);
+        }
 
-        baseRectangle.setAttribute("x", this.rectangle.x);
-        baseRectangle.setAttribute("y", this.rectangle.y);
-        baseRectangle.setAttribute("width", this.rectangle.width);
-        baseRectangle.setAttribute("height", this.rectangle.height);
-        baseRectangle.setAttribute("fill", `rgb(128, 0, 128)`)
+        this.baseRectangle.setAttribute("x", this.rectangle.x);
+        this.baseRectangle.setAttribute("y", this.rectangle.y);
+        this.baseRectangle.setAttribute("width", this.rectangle.width);
+        this.baseRectangle.setAttribute("height", this.rectangle.height);
+        this.baseRectangle.setAttribute("fill", `rgb(128, 0, 128)`)
 
-        loadingCircle.setAttribute("cx", this.loadingcircle.x)
-        loadingCircle.setAttribute("cy", this.loadingcircle.y)
-        loadingCircle.setAttribute("r", this.loadingcircle.r)
+        this.loadingCircle.setAttribute("cx", this.loadingcircle.x)
+        this.loadingCircle.setAttribute("cy", this.loadingcircle.y)
+        this.loadingCircle.setAttribute("r", this.loadingcircle.r)
 
-        dialCircle.setAttribute("cx", this.dialcircle.x)
-        dialCircle.setAttribute("cy", this.dialcircle.y)
-        dialCircle.setAttribute("r", this.dialcircle.r)
+        this.dialCircle.setAttribute("cx", this.dialcircle.x)
+        this.dialCircle.setAttribute("cy", this.dialcircle.y)
+        this.dialCircle.setAttribute("r", this.dialcircle.r)
 
-        this.washingZone.appendChild(baseRectangle);
-        this.washingZone.appendChild(loadingCircle);
-        this.washingZone.appendChild(dialCircle);
+        this.washingZone.appendChild(this.baseRectangle);
+        this.washingZone.appendChild(this.loadingCircle);
+        this.washingZone.appendChild(this.dialCircle);
     }
 
     agument() {
-        this.loadingcircle.x = this.loadingcircle.x + (Math.random() * 100)
+
     }
 }
 
 class WasherManager {
     WashingMachineArray = [];
+    loadingcircle = {x:250, y:350, r:60};
+    dialcircle = {x:350, y:175, r:20};
+    rectangle = {x:100, y:100, width:300, height:400};
+    MachinePlacer = 0;
     MakeMachines() {   
-        for(i = 0; i < 10; i++) {
-            NewMachine = new WashingMachine();
-            WashingMachineArray[i] = NewMachine;
-        }
-        for(i = 0; i < 10; i++) {
-            NewMachineArray[i].agument();
-            NewMachineArray[i].draw();
+        for(var i = 0; i < 10; i++) {
+            var NewMachine = new WashingMachine(this.loadingcircle, this.dialcircle, this.rectangle);
+            this.WashingMachineArray[i] = NewMachine;
         }
     }   
+
+    NextMachine() {
+        this.MachinePlacer += 1;
+        this.WashingMachineArray[this.MachinePlacer].draw();
+    }
+    
+    PreviousMachine() {
+        this.MachinePlacer -= 1;
+        this.WashingMachineArray[this.MachinePlacer].draw();
+    }
 }
 
 let g = new WasherManager();
 g.MakeMachines();
+
+var nextbutton = document.getElementById('next')
+
+function next() {
+    g.NextMachine()
+}
+
+function previous() {
+    g.PreviousMachine()
+}
